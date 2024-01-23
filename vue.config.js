4106c25bd3ee4ac8087de11ff892649e07cf4b5d
yunhodev <yunhodev@gmail.com>
2024-01-23 14:59:05 +0900

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pwa: {
+    name: 'fp_pwa_with_ruffle'
+  }
 })
